f97ea59d52127c17bed9e767b752c649e9589f8f
Archit Agrawal <archit18221@iiitd.ac.in>
2020-03-29 01:50:09 +0530

    Add mouse recording feature

--- webApp/script.js
+++ webApp/script.js
@@ -1,13 +1,16 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
- 
+
 var painting = document.getElementById('paint');
 var paint_style = getComputedStyle(painting);
 canvas.width = parseInt(paint_style.getPropertyValue('width'));
 canvas.height = parseInt(paint_style.getPropertyValue('height'));
 
 var mouse = {x: 0, y: 0};
- 
+var movements =  new Array();
+var t0 = performance.now();
+
+
 canvas.addEventListener('mousemove', function(e) {
   mouse.x = e.pageX - this.offsetLeft;
   mouse.y = e.pageY - this.offsetTop;
@@ -17,19 +20,39 @@ ctx.lineWidth = 3;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.strokeStyle = '#00CC99';
- 
+
 canvas.addEventListener('mousedown', function(e) {
     ctx.beginPath();
     ctx.moveTo(mouse.x, mouse.y);
- 
+
     canvas.addEventListener('mousemove', onPaint, false);
 }, false);
- 
+
 canvas.addEventListener('mouseup', function() {
     canvas.removeEventListener('mousemove', onPaint, false);
 }, false);
- 
+
 var onPaint = function() {
     ctx.lineTo(mouse.x, mouse.y);
+    //console.log(mouse.x, mouse.y);
     ctx.stroke();
+    var t1 = performance.now();
+    var move = {x: mouse.x, y: mouse.y, t:t1-t0};
+    movements.push(move);
 };
+
+function saveMouse() {
+
+
+    return 0;
+  }
+
+
+function download( name, type) {
+    var a = document.getElementById("a");
+    var toStore = JSON.stringify( movements );
+    a.style.display = "block";
+    var file = new Blob([toStore], {type: type});
+    a.href = URL.createObjectURL(file);
+    a.download = name;
+  }
