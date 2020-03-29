201d248f5a28e6979b15fa6dc8d4f55ba5e34138
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2020-03-29 14:27:43 +0530

    Working add slide functionality

--- webApp/decl.js
+++ webApp/decl.js
@@ -1,10 +1,12 @@
 var current_canvas;
-var canvas_list;
+var canvas_dict = {};
 var mouse = {x: 0, y: 0};
+var num_slides = 0;
 
 function new_slide(){
-	var new_canvas = $('<canvas/>', {"style":"border: solid 5pt blue", "width":800, "height":600}).get(0);
-	canvas_list.push(new_canvas);
+	var slide_id = "slide" + (num_slides++);
+	var new_canvas = $('<canvas/>', {"style":"border: solid 5pt blue", "width":800, "height":600, "id":slide_id}).get(0);
+	canvas_dict[slide_id] = new_canvas;
 	$("#canvas_list").append(new_canvas);
 	new_canvas.style.display = 'none'; 
 	var ctx = new_canvas.getContext('2d');
@@ -16,26 +18,23 @@ function new_slide(){
 	ctx.strokeStyle = '#00CC99';
 	var switch_btn = $('<button/>').text('Test').click(
 		function () {
-			set_current(new_canvas);
+			set_current(slide_id);
 		}
 	);
 	$("#slides_list").append(switch_btn);
-	return new_canvas;
+	return slide_id;
 }
 
-function set_current(target_canvas){
+function set_current(slide_id){
 	if(typeof current_canvas !== 'undefined'){
 		current_canvas.style.display = 'none';
+		current_canvas.removeEventListener('mousemove', updateMousePos, false);
+		current_canvas.removeEventListener('mousedown', draw_start, false);	
+		current_canvas.removeEventListener('mouseup', draw_stop, false);
 	}
-	target_canvas.style.display = 'block';
-	current_canvas = target_canvas;
-	current_canvas.addEventListener(
-		'mousemove',
-		function(e) {
-			updateMousePos(current_canvas, e);
-			//console.log(mouse);
-		},
-		false);
+	canvas_dict[slide_id].style.display = 'block';
+	current_canvas = canvas_dict[slide_id];
+	current_canvas.addEventListener('mousemove', updateMousePos, false);
 	current_canvas.addEventListener('mousedown', draw_start, false);	
 	current_canvas.addEventListener('mouseup', draw_stop, false);
 }
@@ -57,8 +56,8 @@ var draw_start = function(e) {
 	current_canvas.addEventListener('mousemove', onPaint, false);
 };
 
-function updateMousePos(canvas, evt) {
-	var rect = canvas.getBoundingClientRect();
+function updateMousePos(evt) {
+	var rect = current_canvas.getBoundingClientRect();
     mouse.x = evt.clientX - rect.left;
     mouse.y = evt.clientY - rect.top;
 }
